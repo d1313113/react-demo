530b78a187479aaf3b16682b706acde5dcacbe04
杜灿城 <ducancheng@mzjmedia.net>
2018-08-29 00:06:41 +0800

    首页头部完成

--- src/views/index/Index.js
+++ src/views/index/Index.js
@@ -6,10 +6,26 @@ import {Header} from "./Header/header";
 import avtar from "imgRoot/index/avtar.png";
 
 class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      header: {
+        imgSrc: avtar,
+        currentWeight: 123,
+        compare: 2,
+        bodyMsg: [
+          {name: "目标周期", val: "14天"},
+          {name: "目标体重", val: "110斤"},
+          {name: "BMI", val: "23.9"},
+          {name: "体型", val: "正常"}
+        ]
+      }
+    };
+  }
   render() {
     return (
       <div className="Index">
-        <Header title={"123"} imgSrc={avtar} />
+        <Header {...this.state.header}/>
       </div>
     );
   }
