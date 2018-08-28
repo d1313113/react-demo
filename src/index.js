eac62764f3198267bde42a8bea3f53cab1595dab
杜灿城 <ducancheng@mzjmedia.net>
2018-08-28 13:54:07 +0800

    添加reset.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'reset-css';
 import './index.css';
 import App from './pages/app/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
