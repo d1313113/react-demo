df594936eef62de4e8ab68882b97e58cd1a15a7e
杜灿城 <ducancheng@mzjmedia.net>
2018-08-30 16:31:37 +0800

    首页tabs样式编写,未添加展开关闭切换

---
+++ src/views/index/Tabs/Listitem.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Listitem.scss";
+
+function Listitem(props) {
+  const requireContext = require.context("imgRoot/index/", false, /\/tabs\d+\.png$/);
+  const listItemImgs = requireContext.keys().map(requireContext);
+  // console.log(props);
+
+  return (
+    <React.Fragment>
+      {
+        props.foodAndSportDescription.map((item,index) => {
+          return (
+            <div className="list-item-box" key={item.title}>
+              <div className="list-item">
+                <img src={listItemImgs[index]} alt="" className="list-item-img" />
+                <div className="title-box">
+                  <div className="title">
+                    {item.title}
+                    <span className="tips">{item.tips}</span>
+                  </div>
+                  <div className="description">{item.description}</div>
+                </div>
+                <div className="cal">
+                  {
+                    item.expand ?
+                    (<React.Fragment>
+                      <span className="num">{item.totalCal}</span>千卡
+                    </React.Fragment>) :
+                    (<img src={require("imgRoot/index/arrow-down.png")} alt="" className="arrow-down" />)
+                  }
+                </div>
+              </div>
+              <div className="detail-box">
+                <div className="detail-item">
+                  <div className="detail-left">
+                    <div className="name">牛奶</div>
+                    <div className="quantity">1袋</div>
+                  </div>
+                  <div className="detail-right">108千卡</div>
+                </div>
+                <div className="switch-item">
+                  <div className="switch-left" style={{"borderRight":"1px solid #e5e5e5"}}>
+                    <img src={listItemImgs[5]} alt=""/>
+                    食物估量
+                  </div>
+                  <div className="switch-right">
+                    <img src={listItemImgs[6]} alt=""/>
+                    换一换
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      }
+    </React.Fragment>
+  );
+}
+
+export default Listitem;
--- src/views/index/Tabs/tabs.js
+++ src/views/index/Tabs/tabs.js
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import "./tabs.scss";
 import { Tabs } from "antd-mobile";
+import Listitem from "./Listitem";
 
-export default function TabsFoodSport() {
-  const tabs = [
-    { title: "First Tab" },
-    { title: "Second Tab" }
-  ];
-  return (
-    <div>
-      <Tabs
-        tabs={tabs}
-        initialPage={1}
-      >
-      </Tabs>
-    </div>
-  );
+class TabsFoodSport extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      test: []
+    };
+  }
+  render() {
+    const tabsTitle = [
+      { title: "饮食", sub: "1" },
+      { title: "运动", sub: "2" }
+    ];
+    const foodDescription = [
+      {
+        title: "早餐",
+        tips: "(早餐要吃好)",
+        description: "以优质蛋白为主",
+        totalCal: 2226,
+        expand: true
+      },
+      {
+        title: "午餐",
+        tips: "(午餐要吃饱)",
+        description: "肉类以白肉为主",
+        totalCal: 2226,
+        expand: false
+      },
+      {
+        title: "加餐",
+        tips: "(加餐要吃精)",
+        description: "补充高蛋白食品",
+        totalCal: 2226,
+        expand: false
+      },
+      {
+        title: "晚餐",
+        tips: "(加餐要吃少)",
+        description: "用粗粮代替主食",
+        totalCal: 2226,
+        expand: false
+      }
+    ];
+    const sportDescription = [
+      {
+        title: "运动",
+        tips: "",
+        description: "无氧+有氧,身材fitfitfit~",
+        totalCal: 2226,
+        expand: true
+      }
+    ];
+
+    return (
+      <div className="tabs-box">
+        <Tabs
+          tabs={tabsTitle}
+          initialPage={0}
+          className="tabs-head"
+          tabBarUnderlineStyle={{borderColor: "#f9940b"}}
+        >
+          <div className="tabs-body">
+            <Listitem foodAndSportDescription={foodDescription} />
+          </div>
+          <div className="tabs-body">
+            <Listitem foodAndSportDescription={sportDescription} />
+          </div>
+        </Tabs>
+      </div>
+    );
+  }
 }
+
+export default TabsFoodSport;
