b3edaac579b760ef4e41ec14c8e02b2d32469f22
dcc <cumelmell@foxmail.com>
2018-09-24 22:45:30 +0800

    新增api请求

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,17 @@
 import request from "../utils/request";
 
 // console.log(get);
-
+/**
+ * 获取用户注册信息
+ */
 export const getRegister = params => request.get("/v113/userinfo/register", params);
+/**
+ * 获取默认饮食
+ * 1.havedrinType 1早餐 2午餐 3晚餐 4加餐 5运动。
+ */
+export const getDefaultFood = params => request.get("/dietsport/v113/default", params);
+/**
+ * 获取目标管理
+ * 1.unionid
+ */
+export const getManagementid = params => request.get("/weightmanagement/v11/managementid", params);
